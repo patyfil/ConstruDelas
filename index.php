078bf0c89539bff42b9dc926457409f430f25da2
Patrícia Maria de Souza <patyfil@gmail.com>
2021-10-02 18:20:22 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aprendendo básico de CSS</title>
+    <link rel="stylesheet" href="estilo.css" type="text/css">
+</head>
+<body class="tela-azul">
+    Aqui vai o código HTML que fará seu site aparecer.
+</body>
+</html>
+
+
